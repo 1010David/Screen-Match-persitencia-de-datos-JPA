909794a739cf78c62a94bf8e4f5fda66329f6a4a
David <j.david.beltran.m@gmail.com>
2024-11-02 11:20:46 -0500

    Modificado ScreenmatchApplication

--- src/main/java/com/cursoalura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/cursoalura/screenmatch/ScreenmatchApplication.java
@@ -1,15 +1,11 @@
 package com.cursoalura.screenmatch;
 
 
-import com.cursoalura.screenmatch.principal.EjemploStreams;
 import com.cursoalura.screenmatch.principal.Principal;
-import com.cursoalura.screenmatch.service.ConsumoAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
 
 
 @SpringBootApplication
